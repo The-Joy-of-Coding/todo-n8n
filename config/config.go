c2b713d542297f1cf94103491233af0c8bdf9f14
yasasvi <yasasvigumma@gmail.com>
2026-01-24 17:06:17 +0530

    Tested ApiUrl in config
    We are applying TDD to develop features.

--- config/config.go
+++ config/config.go
@@ -1,7 +1,46 @@
 package config
 
-import "os"
+import (
+	_ "embed"
+	"log/slog"
+	"os"
+	"strings"
+)
 
-func ApiUrl() string {
-	return os.Getenv("API_URL")
+type ConfigDefault struct {
+	URL string `json:"url"`
 }
+
+var Default ConfigDefault
+
+func init() {
+	setDefaults()
+	setLogger()
+}
+
+func ApiUrl(logger *slog.Logger, url_key string) string {
+	url := os.Getenv(url_key)
+	if url == "" {
+		logger.Error("No valid url found!")
+		return Default.URL
+	}
+	if !strings.HasPrefix(url, "https://") {
+		logger.Warn("Using insecure api endpoint.")
+	}
+	if !ping(url) {
+		logger.Error("Url is invalid, please change the url to run the application.")
+		return Default.URL
+	}
+	Default.URL = url
+	return url
+}
+
+func ping(url string) bool {
+	return url != ""
+}
+
+func setDefaults() {
+	Default.URL = "https://default"
+}
+
+func setLogger() {}
--- config/config_test.go
+++ config/config_test.go
@@ -1,18 +1,37 @@
 package config
 
 import (
+	"bytes"
+	"log/slog"
 	"strings"
 	"testing"
 )
 
 func TestApiUrl(t *testing.T) {
-	result := ApiUrl()
-
-	if result == "" {
-		t.Errorf("ApiUrl returned an empty string!")
+	type Test struct {
+		name     string
+		inputUrl string
+		matchLog string
 	}
-
-	if !strings.HasPrefix(result, "https://") {
-		t.Errorf("ApiUrl() = %q; want it to start with 'https://'", result)
+	tests := []Test{
+		{"EMPTY URL", "", "No valid url found!"},
+		{"INSECURE URL", "INSECURE_API", "Using insecure api endpoint."},
+		{"SECURE URL", "SECURE_API", ""},
+	}
+	testFunc := func(test Test, t *testing.T) {
+		var buf bytes.Buffer
+		l := slog.New(slog.NewJSONHandler(&buf, nil))
+		res := ApiUrl(l, test.inputUrl)
+		if !strings.Contains(buf.String(), test.matchLog) {
+			t.Errorf("Missing log \"%s\" to indicate user!", test.matchLog)
+		}
+		if res == "" {
+			t.Errorf("Returning empty result!")
+		}
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testFunc(test, t)
+		})
 	}
 }
