58c06ea24de9ea387acf8e4306b4a00c910d1225
yasasvi <yasasvigumma@gmail.com>
2026-01-29 22:24:13 +0530

    Added struct and functions for todo
    need to test and complete the default function.

--- module/module.go
+++ module/module.go
@@ -1,7 +1,31 @@
 package module
 
-type todos struct{}
-
-func New() todos {
-	return todos{}
+type Todos struct {
+	Id   int    `json:"id"`
+	Task string `json:"task"`
 }
+
+var todos []Todos
+
+func init() {
+	todo := Todos{}
+	todo.get()
+}
+
+func AddTask(task string) []Todos {
+	todo := Todos{Task: task}
+	todo.post()
+	return todos
+}
+
+func CheckOrDeleteTask(id int, isCheck bool) []Todos {
+	todo := Todos{Id: id}
+	if isCheck {
+		todo.put()
+	} else {
+		todo.delete()
+	}
+	return todos
+}
+
+func Default() {}
