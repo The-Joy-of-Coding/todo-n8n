850119eefc450f767b0f68d69ce302344ce7d3c0
yasasvi <yasasvigumma@gmail.com>
2026-01-30 23:41:35 +0530

    Updated entry files
    just to test the interacion.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	@INSECURE_API=http://localhost SECURE_API=https://example.com go test ./...
+
+run:
+	@go run .
+
+build:
+	@go build .
--- app.go
+++ app.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log/slog"
+
+	"todo-n8n/config"
+)
+
+func main() {
+	apiUrl, err := config.GetEnv("API_URL")
+	header, err := config.GetEnv("AUTH_HEADER")
+	key, err := config.GetEnv("AUTH_KEY")
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	slog.Info(apiUrl)
+	slog.Info(header)
+	slog.Info(key)
+}
