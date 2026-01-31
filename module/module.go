75cfd14a94e26e662fa1bc4f8cc73ee89c65bfa0
yasasvi <yasasvigumma@gmail.com>
2026-01-31 19:22:30 +0530

    Updated code to run the ui
    this will allow us to run the ui and execute the commands.

--- module/module.go
+++ module/module.go
@@ -1,8 +1,17 @@
 package module
 
 import (
+	"context"
+	_ "embed"
+	"fmt"
 	"log/slog"
+	"os"
+	"os/exec"
+	"time"
 )
+
+//go:embed app.sh
+var app_ui string
 
 type Todos struct {
 	Id   int    `json:"id"`
@@ -13,43 +22,49 @@ type N8nRespnce struct {
 	TodoList []Todos `json:"todo_list"`
 }
 
-func getSafeTodos() []Todos {
-	fail := make(chan N8nRespnce)
-	go func(fail chan N8nRespnce) {
-		todo := Todos{}
-		res, err := todo.get()
-		if err != nil {
-			slog.Error(err.Error())
-			fail <- res
-			return
-		}
-		fail <- res
-	}(fail)
-	res := <-fail
-	if len(res.TodoList) == 0 {
-		return nil
+func GetSafeTodos() {
+	todo := Todos{}
+	_, err := todo.get()
+	if err != nil {
+		slog.Error(err.Error())
 	}
-	slog.Info("Todos", "List", res.TodoList)
-	return res.TodoList
 }
 
-func AddTask(task string) []Todos {
+func AddTask(task string) {
 	todo := Todos{Task: task}
 	if err := todo.post(); err != nil {
 		slog.Error(err.Error())
-		return getSafeTodos()
 	}
-	return getSafeTodos()
 }
 
-func CheckOrDeleteTask(id int, isCheck bool) []Todos {
+func CheckTask(id int) {
 	todo := Todos{Id: id}
-	if isCheck {
-		todo.put()
-	} else {
-		todo.delete()
+	if err := todo.put(); err != nil {
+		slog.Error(err.Error())
 	}
-	return getSafeTodos()
 }
 
-func Default() {}
+func DeleteTask(id int) {
+	todo := Todos{Id: id}
+	if err := todo.delete(); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func runner(script string) error {
+	ctx, cancel := context.WithTimeout(
+		context.Background(), time.Minute*5,
+	)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "bash", "-c", script)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func Default() error {
+	script := fmt.Sprintf("%s todo_start", app_ui)
+	slog.Info(script)
+	return runner(script)
+}
