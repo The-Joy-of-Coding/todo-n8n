bc90b51d5af23bda2edecda43360d8a1791c7fec
yasasvi <yasasvigumma@gmail.com>
2026-02-07 19:43:57 +0530

    Added proper header
    This header/title looks cool.

--- module/template/header.sh
+++ module/template/header.sh
@@ -1,4 +1,21 @@
 todo_header() {
-  tput cup 0 0 
-  echo "--- TODO N8N ---"
+  local RED=$'\e[1;31m'
+  local WHITE=$'\e[1;37m'
+  local logo="
+${RED}████████╗  ███████╗  ██████╗   ███████╗
+${RED}╚══██╔══╝ ██╔════██╗ ██╔══██╗ ██╔════██╗
+${RED}   ██║    ██║    ██║ ██║  ██║ ██║    ██║
+${WHITE}   ██║    ██║    ██║ ██║  ██║ ██║    ██║
+${WHITE}   ██║    ╚███████╔╝ ██████╔╝ ╚███████╔╝
+${WHITE}   ╚═╝     ╚══════╝  ╚═════╝   ╚══════╝
+    ${RED}N 8 N  ${WHITE}E D I T I O N"
+  local start_row=1
+  local start_col=2
+  local current_row=$start_row
+  while IFS= read -r line || [[ -n "$line" ]]; do
+    tput cup "$current_row" "$start_col"
+    printf "%s\n" "$line"
+    ((current_row++))
+  done <<< "$logo"
+  printf "${NC}"
 }
