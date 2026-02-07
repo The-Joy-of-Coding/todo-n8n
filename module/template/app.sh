4c6f6a55f0a16cd5f85934a300ba75be84f783d3
yasasvi <yasasvigumma@gmail.com>
2026-02-07 19:41:33 +0530

    Added placeholder functions
    this functions are what will run the ui.

--- module/template/app.sh
+++ module/template/app.sh
@@ -1,10 +1,27 @@
 todo_start() {
   tput civis
   tput clear
-  
-  todo_header
   todo_menu
-  
   tput clear
   tput cnorm  
+}
+
+get_todos() {
+  echo "Getting Todos!"
+}
+
+add_todo() {
+  echo "Added todo!"
+}
+
+check_todo() {
+  echo "check todo!"
+}
+
+delete_todo() {
+  echo "delete todo!"
+}
+
+settings_todo() {
+  echo "go to $N8N_URL"
 }
