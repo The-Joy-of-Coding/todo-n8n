ce9dbc31a67da246b8e8498cc2a22137fd6ce180
yasasvi <yasasvigumma@gmail.com>
2026-02-16 22:33:42 +0530

    Implementing cache structure
    this will technically allow us to reduce the number for requests,
    maybe...

--- module/cache/cache.go
+++ module/cache/cache.go
@@ -1,1 +1,16 @@
 package cache
+
+type cache map[string]any
+
+var (
+	storage cache
+	log     string
+)
+
+func set(data cache) {
+	storage = data
+}
+
+func get() cache {
+	return storage
+}
--- module/cache/cache_test.go
+++ module/cache/cache_test.go
@@ -1,1 +1,9 @@
 package cache
+
+import "testing"
+
+func Test_Set(t *testing.T) {}
+
+func Test_Get(t *testing.T) {}
+
+func Test_Log(t *testing.T) {}
