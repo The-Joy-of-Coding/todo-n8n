601514403392424bb369c73bd71a72599fb65d4e
yasasvi <yasasvigumma@gmail.com>
2026-02-17 23:25:06 +0530

    Added functions to validate and formate
    this will validate and formate the task string.

--- module/fetch/audit.go
+++ module/fetch/audit.go
@@ -1,1 +1,31 @@
 package fetch
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func (t *Todos) Validate() error {
+	words := strings.Fields(t.Task)
+	count := len(words)
+	minCount, maxCount := 5, 50
+	switch {
+	case count < minCount:
+		return fmt.Errorf("The task is shorter then 5 words")
+	case count > maxCount:
+		return fmt.Errorf("The task is longer the 50 words")
+	default:
+		return nil
+	}
+}
+
+func (t *Todos) Formate() {
+	t.Task = strings.TrimSpace(t.Task)
+	if len(t.Task) == 0 {
+		return
+	}
+	task := []rune(t.Task)
+	task[0] = unicode.ToUpper(task[0])
+	t.Task = string(task)
+}
